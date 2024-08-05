77bcb8cf70665ba695c17a6d35293bfb526d9ebe
Carlos Torres <carlos.a.torres.vargas@gmail.com>
2024-08-05 19:47:58 -0400

    Se agrega variables para Test

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -1,11 +1,17 @@
+import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { MyPipelineStack } from '../lib/pipeline-cdk-stack';
+import { AppCdkStack } from '../lib/app-cdk-stack';
+import { MyPipelineStack } from '../lib/pipeline-cdk-stack'
 import { EcrCdkStack } from '../lib/ecr-cdk-stack';
 
 const app = new cdk.App();
 
 const ecrCdkStack = new EcrCdkStack(app, 'ecr-stack', {});
 
-new MyPipelineStack(app, 'MyPipelineStack', {
-    ecrRepository: ecrCdkStack.repository,
+const testCdkStack = new AppCdkStack(app, 'test', {
+  ecrRepository: ecrCdkStack.repository,
+});
+
+const pipelineCdkStack = new MyPipelineStack(app, 'MyPipelineStack', {
+  ecrRepository: ecrCdkStack.repository,
 });
